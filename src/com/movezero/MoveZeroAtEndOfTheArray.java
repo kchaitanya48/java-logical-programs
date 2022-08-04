60ea5ec72b10a040b78f7ece1c499b54c8a3b4d2
krishna chaitanya <kchaitanya.48@outlook.com>
2022-08-05 01:22:01 +0530

    new chagnes

--- src/com/movezero/MoveZeroAtEndOfTheArray.java
+++ src/com/movezero/MoveZeroAtEndOfTheArray.java
@@ -32,8 +32,36 @@ public class MoveZeroAtEndOfTheArray {
 	
 	}
 	
+	//https://www.geeksforgeeks.org/move-zeroes-end-array/
+	static void moveZeroAtEndEx2() {
+		int[] arr= {8,4,0,2,5,0,1,0,9,0,7};
+		int n=arr.length;
+		int count = 0;  // Count of non-zero elements
+		 
+        // Traverse the array. If element encountered is
+        // non-zero, then replace the element at index 'count'
+        // with this element
+        for (int i = 0; i < n; i++)
+            if (arr[i] != 0)
+                arr[count++] = arr[i]; // here count is
+                                       // incremented
+ 
+        // Now all non-zero elements have been shifted to
+        // front and 'count' is set as index of first 0.
+        // Make all elements 0 from count to end.
+        while (count < n)
+            arr[count++] = 0;
+        
+        for(int ab:arr) {
+			System.out.print(ab+" ");
+		}
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
-		moveZeroAtEndEx1();
+		moveZeroAtEndEx2();
 	}
 
 }
