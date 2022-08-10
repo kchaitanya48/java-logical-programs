ff7b682d033c98ba5cd522adf23d7e6134c6e237
krishna chaitanya <kchaitanya.48@outlook.com>
2022-08-10 13:28:35 +0530

    comparator with mutliple fields

--- src/com/comparable/Employee.java
+++ src/com/comparable/Employee.java
@@ -1,27 +1,177 @@
 package com.comparable;
 
-public class Employee implements Comparable<Employee>{
+public class Employee implements Comparable<Employee> {
 
 	private int eId;
-	private String eName;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private double salary;
+	private String city;
+	private String state;
+	
+	
+	
+	
 
-	public Employee(int eId, String eName) {
-		this.eId = eId;
-		this.eName = eName;
+	private Employee(EmployeeBuilder employeeBuilder) {
+		super();
+		this.eId = employeeBuilder.eId;
+		this.firstName = employeeBuilder.firstName;
+		this.lastName = employeeBuilder.lastName;
+		this.age = employeeBuilder.age;
+		this.salary = employeeBuilder.salary;
+		this.city = employeeBuilder.city;
+		this.state = employeeBuilder.state;
 	}
+
+	static public class EmployeeBuilder {
+		private int eId;
+		private String firstName;
+		private String lastName;
+		private int age;
+		private double salary;
+		private String city;
+		private String state;
+
+		public EmployeeBuilder(int eId) {
+			super();
+			this.eId = eId;
+		}
+
+		public EmployeeBuilder setFirstName(final String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
+
+		public EmployeeBuilder setLastName(final String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+
+		public EmployeeBuilder setAge(final int age) {
+			this.age = age;
+			return this;
+		}
+
+		public EmployeeBuilder setSalary(final double salary) {
+			this.salary = salary;
+			return this;
+		}
+
+		public EmployeeBuilder setCity(final String city) {
+			this.city = city;
+			return this;
+		}
+
+		public EmployeeBuilder setState(final String state) {
+			this.state = state;
+			return this;
+		}
+		
+		
+		public Employee build() {
+			return new Employee(this);
+		}
+		
+
+	}
+	
+	
+	
+	
+	
+	
 
 	public int geteId() {
 		return eId;
 	}
 
-	public String geteName() {
-		return eName;
+
+
+
+
+
+
+	public String getFirstName() {
+		return firstName;
 	}
+
+
+
+
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+
+
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+
+
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+
+
+
+
+	public String getState() {
+		return state;
+	}
+
+
+
+
+
+
 
 	@Override
 	public int compareTo(Employee o) {
 		// TODO Auto-generated method stub
-		return this.eId-o.eId;
+		return this.eId - o.eId;
 	}
+
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Employee [eId=" + eId + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
+				+ ", salary=" + salary + ", city=" + city + ", state=" + state + "]";
+	}
+	
+	
+	
 
 }
--- src/com/comparable/TestComparable.java
+++ src/com/comparable/TestComparable.java
@@ -6,22 +6,80 @@ import java.util.List;
 
 public class TestComparable {
 
-	
-	static List<Employee> emp=null;
-	
-	static {
-		Employee emp1=new Employee(122,"krishna");
-		Employee emp2=new Employee(141,"swetha");
-		Employee emp3=new Employee(112,"chaitanya");
-		Employee emp4=new Employee(152,"arvind");
-		Employee emp5=new Employee(102,"kiran");
-		
-		Arrays.asList(new Employee[]{emp1,emp2,emp3,emp4,emp5}).stream().sorted(Comparator.reverseOrder())
-		.forEach(a->System.out.println(a.geteId()+" "+a.geteName()));;
-				
-	}
+
 	
 	public static void main(String[] args) {
+		
+		Employee emp1=new Employee.EmployeeBuilder(1)
+				.setFirstName("krishna")
+				.setLastName("chaitanya")
+				.setAge(25)
+				.setSalary(1344.4)
+				.setCity("PragathiNagar")
+				.setState("Telangana")
+				.build();
+		
+		Employee emp2=new Employee.EmployeeBuilder(1)
+				.setFirstName("swetha")
+				.setLastName("mounika")
+				.setAge(22)
+				.setSalary(2316.23)
+				.setCity("kukatpally")
+				.setState("Telangana")
+				.build();
+		
+		Employee emp3=new Employee.EmployeeBuilder(1)
+				.setFirstName("sarath")
+				.setLastName("bhusarapu")
+				.setAge(30)
+				.setSalary(2316.32)
+				.setCity("bhimavaram")
+				.setState("hyderabad")
+				.build();
+		
+		
+		Employee emp4=new Employee.EmployeeBuilder(1)
+				.setFirstName("kiran")
+				.setLastName("teja")
+				.setAge(35)
+				.setSalary(2443.7)
+				.setCity("bhimavaram")
+				.setState("hyderabad")
+				.build();
+		
+		Employee emp5=new Employee.EmployeeBuilder(1)
+				.setFirstName("krishna")
+				.setLastName("prasad")
+				.setAge(24)
+				.setSalary(1344.4)
+				.setCity("PragathiNagar")
+				.setState("Telangana")
+				.build();
+
+		List<Employee> empList = Arrays.asList(new Employee[] { emp1, emp2, emp3, emp4, emp5 });
+		// single Comparator
+		empList.stream().sorted((e1, e2) -> e1.getFirstName().compareTo(e2.getFirstName()))
+				.forEach(System.out::println);
+		
+
+		// multiple Comparator
+
+		System.out.println("---------------");
+		System.out.println("---------------");
+
+		Comparator<Employee> empComparator = Comparator.comparing(Employee::getFirstName)
+				.thenComparing(Employee::getAge);
+		empList.stream().sorted(empComparator).forEach(System.out::println);
+		
+
+		// multiple Comparator
+
+		System.out.println("---------------");
+		System.out.println("---------------");
+
+		Comparator<Employee> empComparator1 = Comparator.comparing(Employee::getFirstName)
+				.thenComparing(Employee::getAge).thenComparing(Employee::getSalary);
+		empList.stream().sorted(empComparator1).forEach(System.out::println);
 		
 	}
 }
